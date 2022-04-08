437aedd69ac405097dee17ec7dcedc2572208fdf
Ashish M Bhat <61205415+Ashish-M-Bhat@users.noreply.github.com>
2022-04-08 22:24:35 +0530

    Signup feature (#4)
    * Introduced the Signup Feature

--- modules/authentication/login.js
+++ modules/authentication/login.js
@@ -1,33 +1,55 @@
+function loginOrSignup(event){
+    event.preventDefault();
 
+    const apiKey='AIzaSyA357y-kI6368NgHXnMI5pW77y71GqpGuw';
+    const url= event.target.id ==='loginHandler'?'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=': 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=';
 
-const submitLogin = document.getElementById('submitLogin');
-    if(submitLogin)
-    {
-        submitLogin.addEventListener('click', function(event){
-            event.preventDefault();
-            const apiKey='AIzaSyA357y-kI6368NgHXnMI5pW77y71GqpGuw';
-            const url= 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=';
+    const email = event.target.id ==='loginHandler'? loginEmail.value:signupEmail.value;
+    const password = event.target.id ==='loginHandler'? loginPassword.value:signupPassword.value;
 
-            fetch(url+apiKey, {
-                method:'POST',
-                body:JSON.stringify({email:email.value,password:password.value}),
-                headers:{'Content-type':'application/json'}
-              })
-              .then(response => {
-                if(response.ok){
-                  return response.json();
+    fetch(url+apiKey, {
+        method:'POST',
+        body:JSON.stringify({email:email, password:password}),
+        headers:{'Content-type':'application/json'}
+      })
+      .then(response => {
+        if(response.ok){
+          return response.json();
 
-                }
-                else{
-                  alert('fail');
+        }
+        else{
+          alert('fail');
 
-                }
-              })
-              .then(data => {
-                localStorage.setItem('idToken', data.idToken);
-                location.href = "../../";
-              })
-              .catch((e)=> alert(e));
-            });
+        }
+      })
+      .then(data => {
+        localStorage.setItem('idToken', data.idToken);
+        location.href = "../../";
+      })
+      .catch((e)=> alert(e));
+}
+
+const loginHandler = document.getElementById('loginHandler');
+const signupHandler = document.getElementById('signupHandler');
+const createAccountOrAlreadyHaveAccount = document.getElementById('createAccountOrAlreadyHaveAccount');
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+
+loginHandler.addEventListener('click', loginOrSignup);
+signupHandler.addEventListener('click', loginOrSignup);
+
+createAccountOrAlreadyHaveAccount.addEventListener('click', (event)=>{
+    event.preventDefault();
+    let currentPage = getComputedStyle(signupForm).display === 'none'?'loginPage':'signupPage';
+    if(currentPage === 'loginPage'){
+        createAccountOrAlreadyHaveAccount.textContent = 'Already have an Account?';
+        loginForm.style.display = 'none';
+        signupForm.style.display = '';
     }
+    else if(currentPage === 'signupPage'){
+        createAccountOrAlreadyHaveAccount.textContent = 'Create an Acount';
+        loginForm.style.display = '';
+        signupForm.style.display = 'none';
+    }
+});
 

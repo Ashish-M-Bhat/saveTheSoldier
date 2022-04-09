44724bafa9f82443a8a92cf0030a1c17fb0984a0
Ashish M Bhat <61205415+Ashish-M-Bhat@users.noreply.github.com>
2022-04-09 23:50:28 +0530

    Introduced LeaderBoard (#7)
    1. index.html:
                      - Added a new div for leaderboard. It falls under the same menuBar div, to accommodate the tle property;
    2.  leaderBoard.js:
                     - fetches all the points, sorts it and displays the top 5.
                     - Gets called/loaded by the loadModulesWhenPageLoads.js
    3. loadModulesWhenPageLoads
                    - Loads leaderBoard.js

---
+++ modules/Points/leaderBoard.js
@@ -0,0 +1,42 @@
+import fetchAPI from "../fetchAPI.js";
+
+const displayLeaderBoard = (allUsersPointsArray) =>{
+    const leaderBoard = document.querySelector('#leaderBoard');
+
+    let table = document.createElement('table');
+    table.innerHTML = `<tr> <th> User </th> <th> Points </th> </tr>`;
+
+    allUsersPointsArray.forEach(eachpointEntry =>{
+        const tr = document.createElement('tr');
+        tr.innerHTML = `<td>${eachpointEntry.userId}</td><td>${eachpointEntry.points}</td>`;
+        if(localStorage.getItem('userId') && localStorage.getItem('userId')=== eachpointEntry.userId)
+        tr.style.cssText = `background-color:red;`
+        table.appendChild(tr);
+    });
+
+    leaderBoard.appendChild(table);
+}
+(function getUserPoints (){
+
+    const transformData = (dataFromPointsDB) =>{
+        let allUsersPointsArray = [];
+        for(let key in dataFromPointsDB){
+            allUsersPointsArray.push(dataFromPointsDB[key]);
+        }
+        allUsersPointsArray.sort((a,b)=> b.points-a.points);
+        allUsersPointsArray = [...allUsersPointsArray.slice(0,5)];
+        displayLeaderBoard(allUsersPointsArray)
+
+    }
+
+    const requestConfig = {
+        url:'https://react-http-bf239-default-rtdb.firebaseio.com/points.json',
+        method:'GET'
+    };
+
+    const satisfyFetchRequest = fetchAPI(transformData);
+
+    satisfyFetchRequest(requestConfig);
+
+
+})();

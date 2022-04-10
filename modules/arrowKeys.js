0d75a6524e5e39f936c79550f27ba19a6fa3a89e
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2022-04-10 14:46:54 +0530

    Added LeftClickRightClick Movements

--- modules/arrowKeys.js
+++ modules/arrowKeys.js
@@ -2,31 +2,33 @@ import getPositions from "./Positions/getPositions.js";
 
 const player = document.querySelector("#player");
 
-document.addEventListener('keydown',function(event){
-    const keyPressed = event.key;
-    const positions = getPositions();
-    let currentStyleOfPlayer = getComputedStyle(player);
-    switch(keyPressed){
-      case "a":
-      case "A":
-      case "ArrowLeft":
-              // If at Mid, move to Left else if at Right, move to Mid
-              if(Math.abs(parseInt(currentStyleOfPlayer.left) - positions.playerMidPosition) < 10){
-                player.style.left = positions.playerLeftPosition + "px";
-              }
+function movePlayerLeftOrRight(event){
+  event.preventDefault();
+  const positions = getPositions();
+  let currentStyleOfPlayer = getComputedStyle(player);
 
-              else if(Math.abs(parseInt(currentStyleOfPlayer.left) - positions.playerRightPosition) < 10)
-                       player.style.left = positions.playerMidPosition + "px";
-              break;
-      case "d":
-      case "D":
-              case "ArrowRight":
-              // If at Left, move to Mid else if at Mid, move to Right
-              if(Math.abs(parseInt(currentStyleOfPlayer.left) - positions.playerLeftPosition)< 10)
-                    player.style.left = positions.playerMidPosition + "px";
-                    else if(Math.abs(parseInt(currentStyleOfPlayer.left) - positions.playerMidPosition) < 10)
-                        player.style.left = positions.playerRightPosition + "px";
-                break;
+  if(event.key === 'a' || event.key === 'A' || event.key === 'ArrowLeft' || event.type ==='click'){
 
-    }
-  });
+      // If at Mid, move to Left else if at Right, move to Mid
+      if(Math.abs(parseInt(currentStyleOfPlayer.left) - positions.playerMidPosition) < 10)
+        player.style.left = positions.playerLeftPosition + "px";
+
+      else if(Math.abs(parseInt(currentStyleOfPlayer.left) - positions.playerRightPosition) < 10)
+                player.style.left = positions.playerMidPosition + "px";
+  }
+
+  else if(event.key === 'd' || event.key === 'D'|| event.key === 'ArrowRight' || event.type ==='contextmenu'){
+
+      // If at Left, move to Mid else if at Mid, move to Right
+      if(Math.abs(parseInt(currentStyleOfPlayer.left) - positions.playerLeftPosition)< 10)
+        player.style.left = positions.playerMidPosition + "px";
+
+      else if(Math.abs(parseInt(currentStyleOfPlayer.left) - positions.playerMidPosition) < 10)
+          player.style.left = positions.playerRightPosition + "px";
+  }
+
+}
+
+document.addEventListener('keydown',movePlayerLeftOrRight);
+document.addEventListener('click', movePlayerLeftOrRight);
+document.addEventListener('contextmenu', movePlayerLeftOrRight);

175f0ae642d1551cc36207cd2c15fc79625e0621
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2022-04-12 10:42:14 +0530

    styling Changes

--- modules/displayModal.js
+++ modules/displayModal.js
@@ -8,3 +8,16 @@ export default function displayModal(htmlContent, id, timeoutInMs=1000){
 
     document.body.appendChild(modal);
 }
+
+/*
+    const restartButton = document.createElement('button');
+    restartButton.innerHTML = 'Restart?';
+    restartButton.onclick = () => location.reload();
+
+    const closeButton = document.createElement('button');
+    closeButton.innerHTML = 'Close';
+    closeButton.onclick = () => modal.parentNode.removeChild(modal);
+
+    modal.appendChild(restartButton);
+    modal.appendChild(closeButton);
+*/
